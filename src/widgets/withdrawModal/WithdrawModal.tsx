12f2d03de19a35b4bf11bab2d778816f8794a41b
Ilya3dev <ilya3dev@gmail.com>
2023-12-21 08:33:50 +0300

    done

--- src/widgets/withdrawModal/WithdrawModal.tsx
+++ src/widgets/withdrawModal/WithdrawModal.tsx
@@ -1,6 +1,6 @@
 import { FC, useEffect, useState } from "react";
 
-import { useDropdown } from "@/shared/tools";
+import { useDropdown, useMediaQuery } from "@/shared/tools";
 import { CloseIco, USDTIcon } from "@/shared/SVGs";
 
 import s from "./styles.module.scss";
@@ -12,6 +12,7 @@ import prevArr from "@/public/media/common/prevArrow.png";
 import Image from "next/image";
 import { useUnit } from "effector-react";
 import * as ContentModel from "@/widgets/welcomePageSignup/model";
+import { ResultWindow } from "@/shared/ui/ResultWindow/ui/ResultWindow";
 
 interface WithdrawModalProps {}
 
@@ -19,15 +20,31 @@ export const WithdrawModal: FC<WithdrawModalProps> = () => {
   const list = ["BSC", "Tron", "Arbitrum", "Polygon", "Ethereum", "Solana"];
 
   const { open, isOpen, close, dropdownRef } = useDropdown();
+  const {
+    open: errorOpen,
+    isOpen: errorIsOpen,
+    close: errorClose,
+    dropdownRef: errorRef,
+  } = useDropdown();
+  const {
+    open: successOpen,
+    isOpen: successIsOpen,
+    close: successClose,
+    dropdownRef: successRef,
+  } = useDropdown();
 
   const [type, setType] = useState("Tron");
   const [address, setAddress] = useState("");
-  const [amount, setAmount] = useState("");
+  const [amount, setAmount] = useState<any>();
   const [error, setError] = useState(false);
   const [startCall, setStartCall] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [notValidAddress, setNotValidAddress] = useState(false);
 
   const [barerToken] = useUnit([ContentModel.$barerToken]);
 
+  const isMobile = useMediaQuery("(max-width:650px)");
   useEffect(() => {
     if (isOpen) {
       document.documentElement.style.overflow = "hidden";
@@ -42,13 +59,40 @@ export const WithdrawModal: FC<WithdrawModalProps> = () => {
     };
   }, [isOpen]);
 
+  const [notValidAmount, setNotValidAmount] = useState(false);
   const handleWithdraw = () => {
     if (!address || !amount) {
+      if (amount === null) {
+        setAmount("");
+      }
       setError(true);
+    } else if (validateAddress(address) === false || Number(amount) < 100) {
+      if (validateAddress(address) === false) {
+        setAddress("");
+        setNotValidAddress(true);
+      }
+      if (Number(amount) < 100) {
+        setNotValidAmount(true);
+      }
     } else {
       setStartCall(true);
     }
   };
+  useEffect(() => {
+    if (notValidAddress) {
+      setTimeout(() => {
+        setNotValidAddress(false);
+      }, 2000);
+    }
+  }, [notValidAddress]);
+
+  useEffect(() => {
+    if (notValidAmount) {
+      setTimeout(() => {
+        setNotValidAmount(false);
+      }, 2000);
+    }
+  }, [notValidAmount]);
 
   useEffect(() => {
     if (error) {
@@ -63,24 +107,59 @@ export const WithdrawModal: FC<WithdrawModalProps> = () => {
       if (startCall) {
         const reponse = await api.withdraw({
           bareer: barerToken,
-          amount: amount,
+          amount: `${amount}`,
           wallet_address: address?.toLowerCase(),
           network: type,
           token: "USDT",
         });
-
+        setAmount("");
+        setAddress("");
         if (reponse.status === "OK") {
           setStartCall(false);
+          close();
+          setIsSuccess(true);
+          setTimeout(() => {
+            successOpen();
+          }, 1000);
         } else {
           setStartCall(false);
+          close();
+          setIsError(true);
+          setTimeout(() => {
+            errorOpen();
+          }, 1000);
         }
       }
     })();
   }, [startCall]);
 
+  function validateAddress(wallet: string) {
+    const pattern = /^(0x)?[0-9a-fA-F]{40}$/;
+    return pattern.test(wallet);
+  }
+
   return (
     <>
-      {" "}
+      {isError && (
+        <div ref={errorRef}>
+          <ResultWindow
+            close={errorClose}
+            isOpen={errorIsOpen}
+            ref={errorRef}
+            type="error"
+          />
+        </div>
+      )}
+      {isSuccess && (
+        <div ref={successRef}>
+          <ResultWindow
+            close={successClose}
+            isOpen={successIsOpen}
+            ref={successRef}
+            type="success"
+          />
+        </div>
+      )}
       <span className={clsx(s.blur, isOpen && s.blur_active)}></span>
       <div ref={dropdownRef}>
         <button className={s.btn} onClick={open}>
@@ -128,10 +207,15 @@ export const WithdrawModal: FC<WithdrawModalProps> = () => {
               onChange={(el) => setAddress(el.target.value)}
               className={clsx(
                 s.address_input,
-                error && !address && s.error_input
+                error && !address && s.error_input,
+                notValidAddress && s.error_input
               )}
               type="text"
-              placeholder="Enter the withdrawal address"
+              placeholder={
+                notValidAddress
+                  ? "Not a valid address"
+                  : "Enter the withdrawal address"
+              }
             />
           </div>
           <div className={s.about_container}>
@@ -139,14 +223,20 @@ export const WithdrawModal: FC<WithdrawModalProps> = () => {
             <div
               className={clsx(
                 s.amount_block,
-                error && !amount && s.error_input
+                error && !amount && s.error_input,
+                notValidAmount && s.error_input
               )}
             >
               <input
                 value={amount}
-                onChange={(el) => setAmount(el.target.value)}
+                onChange={(el) => {
+                  const amount = parseInt(el.target.value);
+                  // setTokenAmount(amount || null);
+                  setAmount(amount || null);
+                }}
                 className={clsx(s.amount_input)}
-                type="text"
+                type="string"
+                placeholder="Minimum 100"
               />
               <span className={s.amount_usdt}>USDT</span>
               <span className={s.amount_center}>|</span>
